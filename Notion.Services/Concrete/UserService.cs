3005bb0e535260137e8db306c041b1f21c506e79
Cemal  ERDEM <cmlerdm3447@gmail.com>
2020-05-04 13:06:41 +0300

    Revert "Make Static"
    This reverts commit 557387ab9ebf54e2286b9164ca29abcfa0a6bb40.

--- Notion.Services/Concrete/UserService.cs
+++ Notion.Services/Concrete/UserService.cs
@@ -64,7 +64,7 @@ namespace Notion.Services.Concrete
         }
 
 
-        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using var hmac = new System.Security.Cryptography.HMACSHA512();
             passwordSalt = hmac.Key;
