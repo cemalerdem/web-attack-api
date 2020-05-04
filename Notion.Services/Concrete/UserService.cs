557387ab9ebf54e2286b9164ca29abcfa0a6bb40
Cemal  ERDEM <cmlerdm3447@gmail.com>
2020-05-04 13:02:50 +0300

    Make Static

--- Notion.Services/Concrete/UserService.cs
+++ Notion.Services/Concrete/UserService.cs
@@ -64,7 +64,7 @@ namespace Notion.Services.Concrete
         }
 
 
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using var hmac = new System.Security.Cryptography.HMACSHA512();
             passwordSalt = hmac.Key;
